2b22a749b3e937ebc36dd7e13a75cf78f2eb380e
zhiminghan <619341911@qq.com>
2017-02-07 14:48:48 +0800

    创建项目

---
+++ Hello/src/com/bjsxt/Hello.java
@@ -0,0 +1,31 @@
+package com.bjsxt;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// Java 在Eclipse中的操作技巧
+       //Ctrl+1 快捷修复
+		//int a=100L;
+	   //Ctrl+D 快捷删除行
+	   //Shift+Enter 快速进入下一行
+		// Alt+上箭头/下箭头    快速移动行
+	   //Ctrl+Alt+上箭头/下箭头   快速复制行
+	   //Ctrl+M 放大工作区
+	   // Alt+/ 补全代码
+	   // Ctrl+/注释代码
+		
+	  	}
+	// /** +enter 生成javadoc 文档
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	 
+			public String add(int a,int b,boolean c){
+				return null;
+			}
+
+}
